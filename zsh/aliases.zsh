28d9a3746355a3f7d44d30a2a16e60a00bcfcf48
aakashd <aakashd@gmail.com>
2020-05-14 15:24:25 +0800

    Adding git aliases and JRUBY config

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,12 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+alias be='bundle exec'
+
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.last 'log -1 HEAD'
